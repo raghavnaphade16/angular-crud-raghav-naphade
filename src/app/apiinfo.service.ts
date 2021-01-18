64549fd59b3598cc00bdf2c8ca9a69a20f15bc29
Raghav <ragnap99@gmail.com>
2021-01-18 10:08:34 +0530

    commit

--- src/app/apiinfo.service.ts
+++ src/app/apiinfo.service.ts
@@ -1,64 +1,64 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class APIInfoService {
-  data:any;  
-  constructor(private http:HttpClient) { }
+  data: any;
+  constructor(private http: HttpClient) {}
 
-  getData():Observable<any>{
-    const url="http://localhost:3000/data";
-    return this.http.get<any>(url)
+  getData(): Observable<any> {
+    const url = 'http://localhost:3000/data';
+    return this.http.get<any>(url);
   }
-  // createData(){
-  
-  public pushData(text: any){
-    
-    const url="http://localhost:3000/data";
-    this.data=JSON.parse(text);
-    console.log(this.data)
-    this.http.post(url,this.data).toPromise().then(
-      d=>{
+  // createData
+
+  public pushData(text: any) {
+    const url = 'http://localhost:3000/data';
+    this.data = JSON.parse(text);
+    console.log(this.data);
+    this.http
+      .post(url, this.data)
+      .toPromise()
+      .then((d) => {
         console.log(d);
-    //    window.location.reload();
-
-      }
-    );
-    alert("Record Saved Successfully");
-    this.tempAlert("close",5000);
+        //    window.location.reload();
+      });
+    alert('Record Saved Successfully');
+    this.tempAlert('', 5000);
   }
-  public pushEditData(text:any){
+  // Edit and delete data
+  //not deleted actual record
+  //change status active to inactive 
+  public pushEditData(text: any) {
     //alert("In pushEditData");
-    const url="http://localhost:3000/data";
-    this.data=JSON.parse(text);
+    const url = 'http://localhost:3000/data';
+    this.data = JSON.parse(text);
     //alert(this.data);
     //console.log(this.data.id);
-    var id=this.data.id;
-    
-    this.http.put("http://localhost:3000/data/"+id,this.data).toPromise().then(
-      d=>{
+    var id = this.data.id;
+
+    this.http
+      .put('http://localhost:3000/data/' + id, this.data)
+      .toPromise()
+      .then((d) => {
         console.log(d);
         //alert("In PUT Call")
         //alert("In PUT Call"+JSON.stringify(d) );
-        
-      }
-    );
-    alert("Record Edited Successfully");
+      });
+    alert('Record Edited Successfully');
     //alert(this.data);
-    this.tempAlert("",5000);
+    this.tempAlert('', 5000);
   }
 
-  tempAlert(msg:any,duration:any)
-    {
-    var el = document.createElement("div");
-    el.setAttribute("style","position:absolute;top:40%;left:20%;background-color:white;");
+  tempAlert(msg: any, duration: any) {
+    var el = document.createElement('div');
+    el.setAttribute('style', '');
     el.innerHTML = msg;
-    setTimeout(function(){
+    setTimeout(function () {
       el.parentNode.removeChild(el);
-    },duration);
+    }, duration);
     document.body.appendChild(el);
-    }
-  
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule,Routes} from '@angular/router';
-import {HttpClientModule} from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CreatedataComponent } from './createdata/createdata.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-const appRoutes:Routes=[
+const appRoutes: Routes = [
   {
-    path:'create',
-    component:CreatedataComponent
+    path: 'create',
+    component: CreatedataComponent,
   },
   {
-    path:'',
-    redirectTo:'create',
-    pathMatch:'full'
+    path: '',
+    redirectTo: 'create',
+    pathMatch: 'full',
   },
   {
-    path:'**',
-    component:PageNotFoundComponent
-  }
-
+    path: '**',
+    component: PageNotFoundComponent,
+  },
 ];
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreatedataComponent,
-    PageNotFoundComponent
-  ],
+  declarations: [AppComponent, CreatedataComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
@@ -37,6 +32,6 @@ const appRoutes:Routes=[
     ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
